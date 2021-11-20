44bef01ecef6f2a27469f499dbae4c949eea15c8
Alexandre Bourquelot <alexandre.bourquelot@epita.fr>
2021-11-20 14:51:03 +0100

    first tests

---
+++ test.py
@@ -0,0 +1,40 @@
+import PySimpleGUI as sg
+
+theme = 'Native'
+sg.LOOK_AND_FEEL_TABLE['Native'] = {
+    'BACKGROUND': sg.COLOR_SYSTEM_DEFAULT,
+    'TEXT':       sg.COLOR_SYSTEM_DEFAULT,
+    'INPUT':      sg.COLOR_SYSTEM_DEFAULT,
+    'TEXT_INPUT': sg.COLOR_SYSTEM_DEFAULT,
+    'SCROLL':     sg.COLOR_SYSTEM_DEFAULT,
+    'BUTTON':    (sg.COLOR_SYSTEM_DEFAULT, sg.COLOR_SYSTEM_DEFAULT),
+    'PROGRESS':   sg.DEFAULT_PROGRESS_BAR_COLOR,
+    'BORDER': 1,
+    'SLIDER_DEPTH': 0,
+    'PROGRESS_DEPTH': 0,
+}
+if theme == 'Native':
+    sg.wx.NO_BORDER = 0  # No styling
+    sg.DEFAULT_FONT = ("", -1)
+else:
+    sg.wx.NO_BORDER = sg.wx.BORDER_NONE
+    sg.DEFAULT_FONT = ("Helvetica", 10)
+sg.theme(theme)   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('Some text on Row 1')],
+            [sg.Text('Enter something on Row 2'), sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Cancel')],
+            [sg.Table([])] ]
+
+# Create the Window
+window = sg.Window('Window Title',
+                    layout,
+                    no_titlebar=True)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    print('You entered ', values[0])
+
+window.close()
